08c8b9833b1d7d152e2788cbcd1fbc435fc02894
Massart Nathan <220747@CCM-Asty-Moulin.local>
2023-09-19 14:41:39 +0200

    cercle

---
+++ NathanM_ActPOO2/NathanM_ActPOO2/Cercle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NathanM_ActPOO2
+{
+	class Cercle
+	{
+		private double _rayon;
+
+		public double Rayon
+		{
+			get { return _rayon; }
+			set { _rayon = value; }
+		}
+		public Cercle(double rayon) { 
+		
+			_rayon = rayon;
+		
+		}
+		public double CalculerAire()
+		{
+			double aire = (double)(_rayon *_rayon + Math.PI);
+			return aire;
+		}
+		public double CalculerPerimetre() 
+        {
+			double perimetre = (double)(2* _rayon + Math.PI);
+			return perimetre;
+        }
+		public string AfficherCaractéristiques()
+		{
+			double perimetre = this.CalculerPerimetre();
+			double aire = this.CalculerAire();
+			string chaine = " Le cercle de rayon " + _rayon + " a un perimetre de " + perimetre + " et une aire de " + aire +".";
+			return chaine;	
+		}
+	}
+}
---
+++ NathanM_ActPOO2/NathanM_ActPOO2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bienvenue dans ce programme sur le cercle !");
+namespace Cercle
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Quel est le rayon de votre cercle ?");
+			double rayon =Console.ReadLine();
+		}
+	}
+}
