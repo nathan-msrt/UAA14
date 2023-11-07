6499adcbf3d4c50d71d1b2ed58ecc379dd402c2c
Massart Nathan <220747@CCM-Asty-Moulin.local>
2023-11-07 14:48:19 +0100

    fini

---
+++ NathanM_ActPoo3/NathanM_ActPoo3/Elephant.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NathanM_ActPoo3
+{
+    internal class Elephant
+    {
+		private string _nom;
+
+		public string Nom
+		{
+			get { return _nom; }
+			set { _nom = value; }
+		}
+
+		private uint _tailleOreilles;
+
+		public uint TaillleOreilles
+		{
+			get { return _tailleOreilles; }
+			set { _tailleOreilles = value; }
+		}
+        public Elephant(string nom, uint tailleOreille)
+        {
+            _tailleOreilles = tailleOreille;
+            _nom = nom;
+        }
+
+
+        public void EcouteMessage(string message, Elephant quiDit)
+        {
+            Console.WriteLine(_nom + " a entendu un message \n" + quiDit._nom + " a dit : " + message);
+        }
+        public void EnvoieMessage(string message, Elephant quiRecoit)
+        {
+            quiRecoit.EcouteMessage(message, this);
+        }
+		public string AfficheQuiJeSuis()
+		{
+			string Jesuis = "Je m'appelle " + _nom + ".\nMes oreilles mesurent "+ _tailleOreilles +"cm.";
+			return Jesuis;	
+		}
+
+    }
+}
---
+++ NathanM_ActPoo3/NathanM_ActPoo3/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Numerics;
+
+namespace NathanM_ActPoo3
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            string rep = "";
+
+            Elephant zazou = new Elephant("Zazou", 82);
+            Elephant titi = new Elephant("Titi", 100);
+            Console.WriteLine("");
+            Console.WriteLine("Bienvenue dans cet échange d'éléphants \n  1. - Pour Afficher les informations de Zazou \n  2. - Pour afficher les informations de Titi  \n  3. - Pour échanger les informations  \n  4. - Pour voir le transfert d'un message de Zazou à Titi  \n  5. - Pour réviser la notion de tableaux et l'appliquer à des objets");
+            do
+            {
+                string exercice = Console.ReadLine();
+                switch (exercice)
+                {
+                    case "1":
+                        Console.WriteLine(zazou.AfficheQuiJeSuis());
+                        break;
+
+                    case "2":
+                        Console.WriteLine(titi.AfficheQuiJeSuis());
+                        break;
+
+                    case "3":
+                        Elephant tampon = zazou;
+                        zazou = titi;
+                        titi = tampon;
+
+                        Console.WriteLine("Zazou: ");
+                        Console.WriteLine(zazou.AfficheQuiJeSuis());
+                        Console.WriteLine("Titi: ");
+                        Console.WriteLine(titi.AfficheQuiJeSuis());
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Quel est votre message ?");
+                        string message = Console.ReadLine();
+                        zazou.EnvoieMessage(message, titi);
+                        
+                        break;
+
+                    case "5":
+                        Elephant[] elephants = new Elephant[7];
+                        elephants[0] = zazou;
+                        elephants[1] = titi;
+                        elephants[2] = new Elephant("Adam", 147);
+                        elephants[3] = new Elephant("Alexandre", 10);
+                        elephants[4] = new Elephant("Sefedin", 58);
+                        elephants[5] = new Elephant("Jayson", 96);
+                        elephants[6] = new Elephant("Aymeric", 69);
+
+                        // Déterminer l'éléphants avec les + grandes oreilles
+                        Elephant? max = null;
+                        for (int i = 0; i < elephants.Length; i++)
+                        {
+                            Elephant elephant = elephants[i];
+                            if (max == null || elephant.TaillleOreilles > max.TaillleOreilles)
+                            {
+                                max = elephant;
+                            }
+                        }
+
+                        // Vérifier que l'éléphant n'est pas null
+                        if (max != null)
+                        {
+                            Console.WriteLine("L'éléphant qui a les plus grandes oreilles est " + max.Nom + " avec oreilles de " + max.TaillleOreilles + "cm.");
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Cet échange n'existe pas");
+                        break;
+                }
+                Console.WriteLine("Voulez-vous recommencer ? (Tapez sur espace)");
+                rep = Console.ReadLine();
+            } while (rep == "");
+        }
+    }
+}
