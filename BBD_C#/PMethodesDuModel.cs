79f51dc20aaea1ebd44b0293f72c597148475da5
Massart Nathan <220747@CCM-Asty-Moulin.local>
2024-01-23 16:15:05 +0100

    oeee

---
+++ BBD_C#/PMethodesDuModel.cs
@@ -0,0 +1,86 @@
+﻿using MySql.Data.MySqlClient;
+using Mysqlx.Connection;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBD_C_
+{
+    internal class PMethodesDuModel
+    {
+        static string DefinirCheminBD() // détermine la chaîne de connexion
+        {
+            return "server=localhost;database=immo;port=3306;User Id=root;password=root";
+        }
+        public bool ChercheUtilisateurs(out DataSet infos)
+        {
+            bool ok = false;
+            MySqlConnection maConnection = new MySqlConnection(DefinirCheminBD());
+            string query = "";
+            try
+            {
+                maConnection.Open();
+
+                query = "SELECT * FROM utlisateurs;";
+
+                MySqlDataAdapter da = new MySqlDataAdapter(query, maConnection);
+                infos = new DataSet();
+                da.Fill(infos, "mesDonnees");
+                if (infos.Tables[0].Rows.Count >= 1)
+                {
+                    ok = true;
+                }
+                maConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+
+        }
+        public bool AjouteUser(string[] donnees)
+        {
+            bool ok = false;
+            MySqlConnection maConnection = new MySqlConnection(DefinirCheminBD());
+            string query = "";
+            try
+            {
+                maConnection.Open();
+                query= "INSERT INTO utilisateurs (nomUser, prenomUser, loginUser, passWordUser,  role) values ( :nomUser, :prenomUser, :loginUser, :passWordUser, :role)";
+                MySqlCommand insertCommand = new MySqlCommand(query, maConnection);
+
+                insertCommand.Parameters.AddWithValue("@nomUser", donnees[0]);
+                insertCommand.Parameters.AddWithValue("@prenomUser", donnees[1]);
+                insertCommand.Parameters.AddWithValue("@loginUser", donnees[2]);
+                insertCommand.Parameters.AddWithValue("@passWordUser", donnees[3]);
+                insertCommand.Parameters.AddWithValue("@role", donnees[4]);
+                if (insertCommand.ExecuteNonQuery() > 0)
+                {
+                    ok = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                throw;
+            }
+        }
+        public bool AfficheDataUsers(DataSet donnees)
+        {
+            string infos = "";
+            for (int i = 0; i < donnees.Tables[0].Rows.Count; i++)
+            {
+                infos += donnees.Tables[0].Rows[i]["nomUser"].ToString() + " | "+
+                         donnees.Tables[0].Rows[i]["prenomUser"].ToString() + " | " +
+                         donnees.Tables[0].Rows[i]["loginUser"].ToString() + " | " +
+                         donnees.Tables[0].Rows[i]["passWordUser"].ToString() + " | " +"\n";
+            }
+            return infos;
+        }
+    }
+}
---
+++ BBD_C#/Program.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Net;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System;
+using System.Data;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BBD_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] donnees = new string[5];
+            DataSet immoUsers;
+            PMethodesDuModel MonModele = new PMethodesDuModel();
+            Console.WriteLine("Listes des Utilisateurs");
+            Console.WriteLine("-----------------------");
+            if (MonModele.ChercheUtilisateurs(out immoUsers))
+            {
+                string listeUser = MonModele.AfficheDataUsers(immoUsers);
+                Console.WriteLine(listeUser);
+            }
+            else
+            {
+                Console.WriteLine("Pb lors de la lecture des données");
+            }
+            Console.WriteLine("Ajout d'un utilisateur :");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Nom ?");
+            donnees[0]= Console.ReadLine
+
+
+            
+           
+            
+
+        }
+    }
+}
